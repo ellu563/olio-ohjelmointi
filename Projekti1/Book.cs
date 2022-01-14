87e682e84ef4106e7f4e75357a658e861592042c
ellu563 <ellu__1992@hotmail.com>
2022-01-14 15:46:51 +0200

    Commit

---
+++ Projekti1/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace BookExercise
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        // empty constructor, jossei oo parametriä
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0; // alustetaan tässä
+        }
+        // constructor with parameter
+        public Book(string title)
+        {
+            this.title = title; // eli tässä talletetaan se aapinen
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 0;
+        }
+        // many parameters
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            // Setid(id);
+            this.price = price;
+        }
+
+        public string GetBookInformation()
+        {
+            return this.author + ", " + this.title + ", " + this.id + "," +this.price;
+        }
+
+        public void SetId(string idValue)
+        {
+            if (idValue.Length == 5) // onko 5 merkkiä pitkä
+            {
+                this.id = idValue;
+            }
+            else
+            {
+                this.id = "not valid";
+            }
+        }
+
+        public void CompareBook(Book book) 
+        {
+            // pitää toteuttaa kaikki eri vaihtoehdot
+            if (this.price > book.price)
+            {
+                Console.WriteLine(this.title + " on kalliimpi kuin " + book.title);
+            }
+            else if (this.price < book.price)
+            {
+                Console.WriteLine(book.title + " on kalliimpi kuin " + this.title);
+            }
+            else
+            {
+                Console.WriteLine(this.title + " on kalliimpi kuin " + book.title);
+            }
+            Console.WriteLine("kutsuva olio" + this.title);
+            Console.WriteLine("parametrinä tuleva olio" + book.title);
+        }
+    }
+}
---
+++ Projekti1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BookExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is book excercise");
+            Book book1 = new Book(); // olio kirjotetaan aina pienellä book1 ja luokka isolla
+            book1.title = "Keppana";
+            book1.SetId("12345"); // tähän käy myös merkkijono kun se on string tyyppinen... abcde
+            Console.WriteLine("Ensimmäinen kirja: " + book1.title);
+
+            Book book2 = new Book("Aapinen"); // tässä annetaan parametrinä title
+            book2.price = 10.00;
+            Console.WriteLine("Toinen kirja : " + book2.title);
+
+            Book book3 = new Book("Kirja", "Pena", "00100", 20.00);
+            Console.WriteLine(book3.GetBookInformation());
+
+            book3.CompareBook(book2);
+
+            // muista tehä commit+push gittii (commit tallentaa koneelle, push vie gittii)
+        }
+    }
+}
